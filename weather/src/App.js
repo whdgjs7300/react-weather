3beb4f717c6fdd3caba668e740f9a2a0e25c406f
whdgjs7300 <112137464+whdgjs7300@users.noreply.github.com>
2023-03-13 17:09:53 +0900

    3/13 weatherSet

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// 1. 앱이 실행되자마자 현재위치 기반의 날씨가 보인다
+// 2. 날씨정보에는 도시, 섭씨, 화씨, 상태 정보가 들어간다
+// 3. 5개의 버튼이 있다 (1개는 현재위치, 4개는 다른 도시)
+// 4. 도시 버튼을 누를 때 마다 도시별 날씨가 나온다
+// 5. 현재위치 버튼을 누르면 현재위치 기반의 날씨가 나온다.
+// 6. 데이터를 들고오는 동안 로딩 스피너가 돈다.
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hihihihi
     </div>
   );
 }
